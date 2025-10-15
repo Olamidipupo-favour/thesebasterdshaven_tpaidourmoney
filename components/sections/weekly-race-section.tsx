5332d60f2ca794ce5b8e482ba386d4318f3b0488
Olamidipupo Favour <dipoagboola2019@gmail.com>
2025-10-15 11:23:27 +0100

    update: fixed featured boxes section

--- components/sections/weekly-race-section.tsx
+++ components/sections/weekly-race-section.tsx
@@ -7,18 +7,18 @@ export function WeeklyRaceSection() {
         <div className="award-innerblock">
           <div className="award-gradient position-relative">
             <span className="moving-box1 motion1 top-motionbox">
-              <img src="./images/box1.png" alt="Box1" className="img-fluid imgshadow" />
+              <img src="https://rillabox.com/images/box1.png" alt="Box1" className="img-fluid imgshadow" />
             </span>
             <span className="moving-box2 motion1 bottom-motionbox small-box">
-              <img src="./images/box2.png" alt="Box2" className="img-fluid imgshadow" />
+              <img src="https://rillabox.com/images/box2.png" alt="Box2" className="img-fluid imgshadow" />
             </span>
             <div className="award-middlebox">
               <div className="row g-xl-0 g-md-3 g-2 align-items-center position-relative">
                 <div className="col-xl-8 col-lg-6 col-md-6 col-12">
-                  <div className="d-flex align-items-center trophy-boxblock ">
-                    <div className="d-lg-flex d-none">
-                      <img src="../../images/trophy-changed.png" alt="" className="trophy-image" />
-                    </div>
+                    <div className="d-flex align-items-center trophy-boxblock ">
+                      <div className="d-lg-flex d-none">
+                        <img src="https://rillabox.com/images/trophy-changed.png" alt="" className="trophy-image" />
+                      </div>
                     <div>
                       <div className="d-flex flex-wrap gap-17 weeklyrace-header align-items-center">
                         <h5 className="winning-raceamount fw-bold mb-0" title="$10,000">
@@ -33,7 +33,7 @@ export function WeeklyRaceSection() {
                 <div className="col-xl-4 col-lg-6 col-md-6 col-12">
                   <div className="race-block ms-xl-auto ms-auto me-xl-0 me-auto">
                     <h3 className="race-title d-flex align-items-center justify-content-center">
-                      <img className="flex-shrink-0" src="/images/timer.svg" alt="Timer" />
+                      <img className="flex-shrink-0" src="https://rillabox.com/images/timer.svg" alt="Timer" />
                     </h3>
                     <div className="d-flex flex-wrap align-items-center justify-content-around timer-block-changed position-relative">
                       <div className="d-flex align-items-center flex-column gap-2">
@@ -46,21 +46,21 @@ export function WeeklyRaceSection() {
                       <div className="d-flex align-items-center flex-column gap-2">
                         <div className="d-flex gap-1">
                           <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>1</span></div>
-                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>5</span></div>
+                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>4</span></div>
                         </div>
                         <p className="mb-0 leaderboard-text">Hours</p>
                       </div>
                       <div className="d-flex align-items-center flex-column gap-2">
                         <div className="d-flex gap-1">
-                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>3</span></div>
-                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>6</span></div>
+                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>2</span></div>
+                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>5</span></div>
                         </div>
                         <p className="mb-0 leaderboard-text">Minutes</p>
                       </div>
                       <div className="d-flex align-items-center flex-column gap-2">
                         <div className="d-flex gap-1">
-                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>1</span></div>
-                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>8</span></div>
+                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>2</span></div>
+                          <div className="race-timer-changed font-26 fw-semibold text-white text-center d-flex align-items-center justify-content-center"><span>9</span></div>
                         </div>
                         <p className="mb-0 leaderboard-text">Seconds</p>
                       </div>
@@ -69,7 +69,7 @@ export function WeeklyRaceSection() {
                     <div className="text-center mt-2">
                       <button type="button" className="outline-btnanimation border-0 w-100 btn btn-link">
                         <span className="d-flex align-items-center justify-content-center gap-2">View Race  
-                          <img src="../../icons/landing/button-right-arrow.svg" alt="right-arroe" />
+                          <img src="https://rillabox.com/icons/landing/button-right-arrow.svg" alt="right-arroe" />
                         </span>
                       </button>
                     </div>
@@ -78,13 +78,13 @@ export function WeeklyRaceSection() {
               </div>
             </div>
             <span className="moving-box3 motion1 top-motionbox top-center-motion-box">
-              <img src="./images/box3.png" alt="Box1" className="img-fluid imgshadow" />
+              <img src="https://rillabox.com/images/box3.png" alt="Box1" className="img-fluid imgshadow" />
             </span>
             <span className="moving-box4 motion1 bottom-motionbox">
-              <img src="./images/box4.png" alt="Box2" className="img-fluid imgshadow addidas-box" />
+              <img src="https://rillabox.com/images/box4.png" alt="Box2" className="img-fluid imgshadow addidas-box" />
             </span>
             <div className="d-lg-none d-flex align-items-center justify-content-center">
-              <img src="../../images/trophy-changed.png" alt="" className="trophy-image z-1" />
+              <img src="https://rillabox.com/images/trophy-changed.png" alt="" className="trophy-image z-1" />
             </div>
           </div>
         </div>
